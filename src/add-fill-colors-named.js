042865353a711d034f859f4c321654d20494e5f4
John McClumpha <john@activepipe.com>
2019-09-18 08:21:30 +1000

    feat: adds ability to use layer names for swatches

---
+++ src/add-fill-colors-named.js
@@ -0,0 +1,38 @@
+import sketch from 'sketch'
+
+export default function(context) {
+
+  const document = sketch.getSelectedDocument()
+  const selectedLayers = document.selectedLayers
+  const selectedCount = selectedLayers.length
+
+  var fillcolors = []
+  var colorAssets = []
+
+  if (selectedCount === 0) {
+    sketch.UI.message('No layers are selected.')
+  } else {
+    selectedLayers.forEach(function (layer, i) {
+      layer.style.fills.forEach(function (fill, j) {
+        if(fill.fill === 'Color'){
+          fillcolors.push({'color':fill.color, 'name':layer.name})
+        }
+      })
+    })
+
+    if(fillcolors.length > 0) {
+      fillcolors.forEach(function(fillcolor, i) {
+        var mscolor = MSImmutableColor.colorWithSVGString(fillcolor.color).newMutableCounterpart()
+        var newcolor = MSColorAsset.alloc().initWithAsset_name(mscolor, fillcolor.name)
+        colorAssets.push(newcolor)
+      })
+      var doc = context.document
+      var assets = doc.documentData().assets()
+      assets.addColorAssets(colorAssets)
+      sketch.UI.message(`${fillcolors.length} colors added.`)
+    } else {
+      sketch.UI.message('No fill colors found')
+    }
+  }
+  
+}
