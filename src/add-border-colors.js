749a09862b933de4ea3fb17ba3fc2e064d4c63ba
John McClumpha <john@activepipe.com>
2019-09-18 08:43:36 +1000

    feat: adds use of border colours

---
+++ src/add-border-colors.js
@@ -0,0 +1,38 @@
+import sketch from 'sketch'
+
+export default function(context) {
+
+  const document = sketch.getSelectedDocument()
+  const selectedLayers = document.selectedLayers
+  const selectedCount = selectedLayers.length
+
+  var fillcolors = []
+  var colorAssets = []
+
+  if (selectedCount === 0) {
+    sketch.UI.message('No layers are selected.')
+  } else {
+    selectedLayers.forEach(function (layer, i) {
+      layer.style.borders.forEach(function (border, j) {
+        if(border.fillType === 'Color'){
+          fillcolors.push(border.color)
+        }
+      })
+    })
+
+    if(fillcolors.length > 0) {
+      fillcolors.forEach(function(fillcolor, i) {
+        var mscolor = MSImmutableColor.colorWithSVGString(fillcolor).newMutableCounterpart()
+        var newcolor = MSColorAsset.alloc().initWithAsset_name(mscolor, fillcolor)
+        colorAssets.push(newcolor)
+      })
+      var doc = context.document
+      var assets = doc.documentData().assets()
+      assets.addColorAssets(colorAssets)
+      sketch.UI.message(`${fillcolors.length} colors added.`)
+    } else {
+      sketch.UI.message('No fill colors found')
+    }
+  }
+  
+}
